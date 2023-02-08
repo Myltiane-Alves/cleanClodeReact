7f6ea3fcfcfa08536e8f3849a4ce914d0548537b
Myltiane <myltiane.aux@gmail.com>
2023-02-07 22:14:35 -0300

    refactor: imports

--- src/domain/test/mock-authentication.ts
+++ src/domain/test/mock-authentication.ts
@@ -1,6 +1,8 @@
 import { faker } from "@faker-js/faker";
 import { AuthenticationParams } from "../useCases/authentication";
 
+
+
 export const mockAuthentication = (): AuthenticationParams => ({
     email: faker.internet.email(),
     password: faker.internet.password()
